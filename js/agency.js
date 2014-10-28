04341ff67eb25d6b829e415bfdb13f25f7188d82
Igor Fernandes <igor.fernandes@corp.globo.com>
2014-10-28 15:48:04 -0200

    Fix offset value

--- js/agency.js
+++ js/agency.js
@@ -9,7 +9,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 50
+            scrollTop: $($anchor.attr('href')).offset().top - 70
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -17,7 +17,7 @@ $(function() {
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    offset: 50,
+    offset: 70,
     target: '.navbar-fixed-top'
 })
 
